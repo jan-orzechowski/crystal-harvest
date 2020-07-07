4a346ce75e66d4a336cd40685008599f01fb1c4c
Jan Orzechowski <orzechowski.jan@gmail.com>
2020-07-07 20:36:58 +0200

    tekst angielski

--- Assets/Scripts/Model/StaticLanguageData.cs
+++ Assets/Scripts/Model/StaticLanguageData.cs
@@ -164,13 +164,150 @@ public static class StaticLanguageData
         else if (language == Language.English)
         {
             return new Dictionary<string, string>()
-            {
-                 // Menu
-                {"s_new_game_button", "New game" },
+            {                                
+                // Menu
+                {"s_new_game_button", "New Game" },
                 {"s_credits_button", "About" },
-                {"s_quit_button", "Exit" },
+                {"s_quit_button", "Quit" },
+                {"s_quit_prompt", "Are you sure you want to quit?" },
+                {"s_retreat_prompt", "Are you sure you want to abandon the mission?" },
+          
+                // O grze
+                {"s_credits_programming", "Design and Programming" },
+                {"s_credits_icons", "Icon Design" },
+                {"s_credits_music", "Music" },
+                {"s_credits_sound", "Sound Effects" },
+                {"s_credits_fonts", "Fonts" },
+                {"s_credits_gfx", "Graphics" },
+                {"s_credits_scripts", "Additional Scripts" },
+                {"s_credits_palette", "Color Palette" },
+                        
+                // Wiadomości
+                {"s_start_text", "Space crystals are the most valuable resource on the intergalactic market. Your mission is to steal them from a desert planet and escape before your presence is detected by its owners." },
+                {"s_victory_text", "You successfully gathered the crystals! Your wealth shall now be unsurpassed.  " },
+                {"s_time_defeat_text", "Time's out, you need to run! Maybe next time..." },
+                {"s_death_defeat_text", "The entire crew is dead! Fortunately there are always more volunteers..." },
+                {"s_start_confirmation", "Let's get to work!" },
+                {"s_victory_confirmation", "Victory!" },
+                {"s_defeat_confirmation", "Too bad." },
 
-                {"s_start_text", "Wersja angielska" },
+                // Zasoby i opisy
+                {"s_crystals", "Crystals" },
+                {"s_crystals_desc", "Crystals. Gathered automatically after you construct the Crystal Storage facility" },
+                {"s_crystals_required", "Crystal Slot" },
+
+                {"s_metal", "Steel" },
+                {"s_metal_desc", "Steel. Required to construct facilities and spare parts" },
+                {"s_metal_required", "Steel Slot" },
+
+                {"s_gas", "Gas" },
+                {"s_gas_desc", "Gas. Extracted in the Refinery" },
+                {"s_gas_required", "Gas Slot" },
+
+                {"s_ore", "Ore" },
+                {"s_ore_desc", "Ore. Gathered automatically by your crew after constructing the Foundry and then smeltered into steel" },
+                {"s_ore_required", "Ore Slot" },
+
+                {"s_plants", "Plants" },
+                {"s_plants_desc", "Plants. Required to produce medicine and food" },
+                {"s_plants_required", "Plant Slot" },
+
+                {"s_medicine", "Medicine" },
+                {"s_medicine_desc", "Medicine. Produced in the Laboratory, required for the functioning of the Healing Chamber" },
+                {"s_medicine_required", "Medicine Slot" },
+
+                {"s_food", "Food" },
+                {"s_food_desc", "Food. Consumed by your astronauts in Living Quarters" },
+                {"s_food_required", "Food Slot" },
+
+                {"s_parts", "Spare Parts" },
+                {"s_parts_desc", "Spare Parts. Used to build and repair robots" },
+                {"s_parts_required", "Spare Parts Slot" },
+
+                {"s_robot", "Robot" },
+                {"s_robot_desc", "Robots" },
+                {"s_robot_required", "Robot Slot" }, 
+
+                // Potrzeby
+                {"Hunger", "Hunger" },
+                {"HungerDesc", "Hunger. Your astronauts can sate it by eating a meal in their Living Quarters" },
+                {"Health", "Health" },
+                {"HealthDesc", "Health. Restored in the Healing Chamber" },
+                {"Condition", "Condition" },
+                {"ConditionDesc", "Condition. Restored in the Repair Station using Spare Parts" },          
+
+                // Budynki
+                {"Spaceship", "Spaceship" },
+                {"OreDeposit", "Ore" },
+                {"CrystalsDeposit", "Crystals" },
+                {"Stairs", "Stairs" },
+                {"Platform", "Platform" },
+                {"Slab", "Pavement" },
+                {"Greenhouse", "Greenhouse" },
+                {"Storage", "Storage" },
+                {"HealingChamber", "Healing Chamber" },
+                {"Quarters", "Living Quarters" },
+                {"Metalworks", "Foundry" },
+                {"Refinery", "Refinery" },
+                {"Laboratory", "Laboratory" },
+                {"FoodSynthesizer", "Food Synthesizer" },
+                {"RobotFactory", "Robot Factory" },
+                {"PartsFactory", "Spare Parts Factory" },
+                {"RepairStation", "Repair Station" },
+                {"CrystalStorage", "Crystal Storage" },            
+
+                // Opisy budynków
+                {"SpaceshipDesc", "" },
+                {"OreDepositDesc", "" },
+                {"CrystalsDepositDesc", "" },
+                {"StairsDesc", "Allows you to reach platforms and rock formations" },
+                {"PlatformDesc", "Gives more space for new buildings" },
+                {"SlabDesc", "Allows for faster movement" },
+                {"GreenhouseDesc", "This is where you grow plants required for the production of food and medicine" },
+                {"StorageDesc", "Allows you to store any resource except for crystals" },
+                {"HealingChamberDesc", "Allows you to improve the health of your astronauts" },
+                {"QuartersDesc", "Here your astronauts can have a meal and some rest" },
+                {"MetalworksDesc", "Smelters ore into steel" },
+                {"RefineryDesc", "This building allows for the extraction of gas. It can only be constructed on sand." },
+                {"LaboratoryDesc", "Used to make medicine for your astronauts" },
+                {"FoodSynthesizerDesc", "Synthesizes food out of plants" },
+                {"RobotFactoryDesc", "Builds robots that assist your astronauts in their work" },
+                {"PartsFactoryDesc", "Produces parts required to build and repair robots" },
+                {"RepairStationDesc", "Used to repair damaged robots" },
+                {"CrystalStorageDesc", "Special storage facility for crystals only" },
+
+                // Budowa
+                {"s_halted", "On Hold" },
+                {"s_scaffolding_construction", "Scaffolding Construction" },
+                {"s_scaffolding_deconstruction", "Scaffolding Demolition" },
+                {"s_construction_site", "Construction Site" },
+                {"s_deconstruction_site", "Demolition Site" },
+
+                {"s_cancel_deconstruction_button", "Cancel Demolition" },
+                {"s_deconstruction_button", "Demolish" },
+                {"s_halt_button", "Hold" },
+                {"s_start_button", "Continue" },
+                {"s_deconstruction_prompt", "Are you sure you want to demolish this building?" },
+
+                // Wskazówki
+                {"s_tips_1", "Controls: \n\n WSAD / arrow keys – camera movement \n Q, E – camera rotation \n – Space – active pause \n R – rotate building                                     " },
+                {"s_tips_2", "Your crew automatically decides what to do and where to go. \n\nTo make sure it is focused on the right tasks, it is recommended to limit the number of concurrent construction sites and to use the option to put a construction on hold.                                    " },
+                {"s_tips_3", "Your crew automatically begins to harvest crystals after you construct the Crystal Storage facility.\n\nIt is best to first ensure that your crew's needs are taken care of and to build a lot of robots to assist them – without them it may be difficult to make it on time.\n\nClicking on a crew member will show their hunger and health levels. If either drops to zero, the crew member dies." },
+                {"s_tips_4", "It is always a good idea to use the active pause to save valuable seconds while considering your next move.              " },
+                {"s_tips_next", "Next" },
+                {"s_tips_end", "OK" },
+
+                // Inne
+                {"s_stats_button", "Stats" },
+                {"s_build_button", "Build" },
+                {"s_sound_on_button", "Sound On" },
+                {"s_sound_off_button", "Sound Off" },
+                {"s_tips_button", "Hints" },
+                {"s_timer_hover", "Time Remaining" },
+                {"s_counter_hover", "Crystals to Harvest" },
+                {"s_humans_desc", "Crew" },
+                {"s_yes", "Yes" },
+                {"s_no", "No" },
             };
         }
         else
